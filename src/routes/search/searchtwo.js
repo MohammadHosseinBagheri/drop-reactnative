3af8c04fb5714dc894c4fbec7188fdbc6ff9adc1
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2020-08-30 01:39:54 +0430

    second search

---
+++ src/routes/search/searchtwo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const SearchTwo = () => {
+    return (
+        <View>
+            <Text>SearchTwo</Text>
+        </View>
+    )
+}
+
+export default SearchTwo
+
+const styles = StyleSheet.create({})
