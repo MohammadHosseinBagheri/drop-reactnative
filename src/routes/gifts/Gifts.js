90f3a268c8b145b43c11ad6bafd3587ca10d73f5
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2020-08-30 01:39:01 +0430

    gift component

---
+++ src/routes/gifts/Gifts.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Gifts = () => {
+    return (
+        <View>
+            <Text>Gifts</Text>
+        </View>
+    )
+}
+
+export default Gifts
+
+const styles = StyleSheet.create({})
