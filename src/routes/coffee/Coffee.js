da070932bf0315a68b9fc55ca41702cb3d2eecc5
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2020-08-30 05:18:53 +0430

    add bg color

--- src/routes/coffee/Coffee.js
+++ src/routes/coffee/Coffee.js
@@ -1,26 +1,25 @@
 import React, {useEffect, useState, useRef} from 'react';
-import {StyleSheet, Text, View, ScrollView, Dimensions} from 'react-native';
+import {StyleSheet, Text, View, Dimensions} from 'react-native';
 import Animated from 'react-native-reanimated';
+import TitleBar from '../../components/title-bar/TitleBar';
+import CoffeeContent from '../../components/coffee-content/CoffeeContent';
 
-const {width, height} = Dimensions.get('window');
+const {width} = Dimensions.get('window');
+const ButtonWidth = (width * 20) / 100;
 
 const Slider = [
   {
-    title: 'slide 1',
+    title: 'All',
     backgroundColor: 'red',
   },
 
   {
-    title: 'slide 2',
+    title: 'PIZZA',
     backgroundColor: 'yellow',
   },
   {
-    title: 'slide 3',
+    title: 'STEAK',
     backgroundColor: 'pink',
-  },
-  {
-    title: 'slide 4',
-    backgroundColor: 'blue',
   },
 ];
 
@@ -29,73 +28,85 @@ const Coffee = () => {
   const [selectedIndex, setSelectedIndex] = useState(0);
 
   const viewRef = useRef();
+  const scrollViewRef = useRef(null);
 
-  const onScroll = Animated.event([{nativeEvent: {contentOffset: {x}}}],{useNativeDriver:true});
-  // (event) => {
+  const onScroll = Animated.event(
+    [{nativeEvent: {contentOffset: {x}}}],
+    {
+      useNativeDriver: true,
+    },
+    (event) => {
+      const contentOffset = event.nativeEvent.contentOffset.x;
+      const viewSize = event.nativeEvent.layoutMeasurement.width;
+      const selectedIndex = Math.floor(contentOffset / viewSize);
+      setSelectedIndex(selectedIndex);
+      console.log(event);
+    },
+  );
+  // const onScroll = (event) => {
+  //   Animated.event([{nativeEvent: {contentOffset: {x}}}], {
+  //     useNativeDriver: true,
+  //   });
   //   const contentOffset = event.nativeEvent.contentOffset.x;
   //   const viewSize = event.nativeEvent.layoutMeasurement.width;
   //   const selectedIndex = Math.floor(contentOffset / viewSize);
   //   setSelectedIndex(selectedIndex);
+  //   console.log;
   // };
   const translateX = x.interpolate({
-    inputRange: Slider.map((_, index) => index * width),
-    outputRange: Slider.map((_, index) => -index * width),
+    inputRange: [0, width, 2 * width],
+    outputRange: [
+      0,
+      -ButtonWidth - (width * 4) / 100,
+      -2 * ButtonWidth - (width * 8) / 100,
+    ],
+    extrapolateLeft: 'clamp',
   });
-  const backgroundColor = x.interpolate({
-    inputRange: Slider.map((_, index) => index * width),
-    outputRange: Slider.map((item, index) => item.backgroundColor),
-  });
-  useEffect(() => console.log(x), [x]);
+  const scrollPress = (index) => {
+    // setSelectedIndex(index);
+    scrollViewRef.current
+      .getNode()
+      .scrollTo({x: width * index, y: 0, animated: true});
+  };
+
   return (
-    <View style={{flex: 1, flexDirection: 'column'}}>
+    <View style={styles.container}>
+      <Animated.View
+        style={[styles.titleBarContainer, {transform: [{translateX}]}]}>
+        {Slider.map((item, index) => (
+          <TitleBar index={index} {...item} scrollPress={scrollPress} />
+        ))}
+      </Animated.View>
       <Animated.ScrollView
+        ref={scrollViewRef}
         {...{onScroll}}
         bounces={false}
         horizontal
         showsHorizontalScrollIndicator={false}
         snapToInterval={width}
         pagingEnabled
-        contentContainerStyle={{height: 20, width: width * Slider.length,backgroundColor:'yellow'}}>
+        contentContainerStyle={styles.contentContainerStyle}>
         {Slider.map((item, index) => (
-          <View
-            style={{
-              flexDirection: 'row',
-              justifyContent: 'center',
-              flex: 1,
-              backgroundColor: 'pink',
-              width: '100%',
-            }}>
-            <Text>{item.title}</Text>
-          </View>
+          <CoffeeContent {...item} index={index} />
         ))}
       </Animated.ScrollView>
-      <View
-        style={{
-          flex: 1,
-          flexDirection: 'row',
-          width: width * Slider.length,
-          backgroundColor:'blue',
-          alignItems:'flex-start',
-          justifyContent:'flex-start'
-        }}>
-        {Slider.map((item, index) => (
-          <Animated.View
-            ref={viewRef}
-            style={{
-              flex: 1,
-              width: '100%',
-              transform: [{translateX}],
-              backgroundColor: item.backgroundColor,
-              height:'100%'
-            }}>
-            <Text>{item.title}</Text>
-          </Animated.View>
-        ))}
-      </View>
     </View>
   );
 };
 
 export default Coffee;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {flex: 1, flexDirection: 'column', backgroundColor: '#37474F'},
+  titleBarContainer: {
+    flexDirection: 'row',
+    marginVertical: 20,
+  },
+  contentContainerStyle: {
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: width * Slider.length,
+    backgroundColor: '#fff',
+  },
+});
