8cb545f9acf28a8a83cc8465c8b4c9417b498850
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2020-08-31 21:00:38 +0430

    swipe up

--- src/components/shopping-cart/ShoppingCart.js
+++ src/components/shopping-cart/ShoppingCart.js
@@ -1,66 +1,30 @@
 import React from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  Dimensions,
-  TouchableOpacity,
-  ScrollView,
-} from 'react-native';
-import Animated from 'react-native-reanimated';
+import {StyleSheet, Text, View,ScrollView} from 'react-native';
+import SwipeUpDown from 'react-native-swipe-up-down';
+
+const ItemMini = () => (
+  <View style={{flex:1}} >
+    <Text>item mini</Text>
+  </View>
+);
+const ItemFull = () => (
+  <ScrollView>
+    <Text>item full</Text>
+    <Text>item full</Text>
+  </ScrollView>
+);
 
 const ShoppingCart = () => {
-  const translateY = new Animated.Value(0);
-  translateY.interpolate(
-    {
-      inputRange: [0, 450],
-      outputRange: [0, 450],
-      extrapolate: 'clamp',
-    },
-    {useNativeDriver: true},
-  );
-  const onScroll = Animated.event([
-    {nativeEvent: {contentOffset: {y: translateY}}},
-  ]);
-  const position = {
-    transform: [
-      {
-        translateY: translateY.interpolate({
-          inputRange: [0, 1],
-          outputRange: [0, 450],
-          extrapolate: 'clamp',
-        }),
-      },
-    ],
-  };
   return (
-    <Animated.View
-      onLayout={(event) => {}}
-      style={{
-        backgroundColor: 'red',
-        height: 50,
-        left: 0,
-        bottom: 0,
-        borderTopLeftRadius: 25,
-        borderTopRightRadius: 25,
-        bottom: 0,
-        transform: [{translateY}],
-        width: Dimensions.get('window').width,
-        zIndex: 20,
-      }}>
-      {/* <TouchableOpacity
-        style={{
-          width: '100%',
-          top: 0,
-          height: 500,
-          zIndex: 40,
-        }}>
-        <Text>hi</Text>
-      </TouchableOpacity> */}
-    </Animated.View>
+    <SwipeUpDown
+      swipeHeight={100}
+      itemMini={<ItemMini />}
+      itemFull={<ItemFull />}
+      onShowFull={() => console.log('full')}
+      onShowMini={() => console.log('mini')}
+    />
   );
 };
-
 export default ShoppingCart;
 
 const styles = StyleSheet.create({});
