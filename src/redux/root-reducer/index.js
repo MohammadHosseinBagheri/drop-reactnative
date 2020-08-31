579f3ffec74917792d453af355cfa5a5583b45e1
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2020-08-31 20:55:29 +0430

    add cart reducer to root-reducer

--- src/redux/root-reducer/index.js
+++ src/redux/root-reducer/index.js
@@ -1,6 +1,8 @@
 import {combineReducers} from 'redux';
 import collectionReducer from '../collections/reducer/collection';
+import cartReducer from '../cart/reducer/cart';
 const reducer = combineReducers({
   collections: collectionReducer,
+  cart: cartReducer,
 });
 export default reducer;
