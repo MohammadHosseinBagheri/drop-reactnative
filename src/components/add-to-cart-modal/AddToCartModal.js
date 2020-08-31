86facba780034afc9488a36a0cf013f3e2a93041
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2020-08-31 20:59:01 +0430

    add cart action

--- src/components/add-to-cart-modal/AddToCartModal.js
+++ src/components/add-to-cart-modal/AddToCartModal.js
@@ -9,6 +9,8 @@ import {
 } from 'react-native';
 import Modal from 'react-native-modalbox';
 import {Button} from 'native-base';
+import {connect} from 'react-redux';
+import {addToCart} from '../../redux/cart/action/cart';
 
 const {width} = Dimensions.get('window');
 
@@ -36,55 +38,55 @@ export class AddToCartModal extends Component {
         backdrop={true}
         position={'center'}
         style={styles.modalContainer}>
-          <View style={styles.viewContainer}>
-            <TouchableOpacity onPress={() => this.close()}>
-              <Text style={styles.closeButton}>Close</Text>
-            </TouchableOpacity>
+        <View style={styles.viewContainer}>
+          <TouchableOpacity onPress={() => this.close()}>
+            <Text style={styles.closeButton}>Close</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.viewContent}>
+          <View style={{flex: 1, marginLeft: 35, marginTop: 10}}>
+            <Text style={{color: 'white', fontSize: 18, fontWeight: 'bold'}}>
+              {this.state.item.name}
+            </Text>
+            <Text style={{color: 'white', fontSize: 12, marginTop: 5}}>
+              {this.state.item.tagline}
+            </Text>
+            <Text style={{color: 'white', fontSize: 12}}>
+              {this.state.item.abv}
+            </Text>
+            <Text
+              numberOfLines={2}
+              style={{color: 'white', fontSize: 12, marginTop: 5}}>
+              {this.state.item.description}
+            </Text>
+            <Text
+              numberOfLines={3}
+              style={{color: 'white', fontSize: 12, marginTop: 8}}>
+              {this.state.item.food_pairing}
+            </Text>
           </View>
-          <View style={styles.viewContent}>
-            <View style={{flex: 1, marginLeft: 35, marginTop: 10}}>
-              <Text style={{color: 'white', fontSize: 18, fontWeight: 'bold'}}>
-                {this.state.item.name}
-              </Text>
-              <Text style={{color: 'white', fontSize: 12, marginTop: 5}}>
-                {this.state.item.tagline}
-              </Text>
-              <Text style={{color: 'white', fontSize: 12}}>
-                {this.state.item.abv}
-              </Text>
-              <Text
-                numberOfLines={2}
-                style={{color: 'white', fontSize: 12, marginTop: 5}}>
-                {this.state.item.description}
-              </Text>
-              <Text
-                numberOfLines={3}
-                style={{color: 'white', fontSize: 12, marginTop: 8}}>
-                {this.state.item.food_pairing}
-              </Text>
-            </View>
-            <View style={{flex: 1, marginTop: 25}}>
-              <View
-                style={{
-                  width: '55%',
-                  height: '55%',
-                  backgroundColor: '#fff',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  alignSelf: 'center',
-                  borderRadius: 15,
-                }}>
-                <Image
-                  resizeMode={'stretch'}
-                  source={{uri: this.state.item.image_url}}
-                  style={{width: '30%', height: '80%'}}
-                />
-              </View>
+          <View style={{flex: 1, marginTop: 25}}>
+            <View
+              style={{
+                width: '55%',
+                height: '55%',
+                backgroundColor: '#fff',
+                justifyContent: 'center',
+                alignItems: 'center',
+                alignSelf: 'center',
+                borderRadius: 15,
+              }}>
+              <Image
+                resizeMode={'stretch'}
+                source={{uri: this.state.item.image_url}}
+                style={{width: '30%', height: '80%'}}
+              />
             </View>
           </View>
-          <Button onPress={() => this.close()} style={styles.addToCartButton}>
-            <Text style={{fontWeight: 'bold'}}>ADD TO CART</Text>
-          </Button>
+        </View>
+        <Button onPress={() => this.close()} style={styles.addToCartButton}>
+          <Text style={{fontWeight: 'bold'}}>ADD TO CART</Text>
+        </Button>
       </Modal>
     );
   }
@@ -117,9 +119,15 @@ const styles = StyleSheet.create({
     position: 'absolute',
     right: 10,
     bottom: 10,
-    paddingHorizontal:30,
+    paddingHorizontal: 30,
     borderRadius: 15,
   },
 });
 
-export default AddToCartModal;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addItemToCart: (item) => dispatch(addToCart(item)),
+  };
+};
+
+export default connect(null, mapDispatchToProps)(AddToCartModal);
