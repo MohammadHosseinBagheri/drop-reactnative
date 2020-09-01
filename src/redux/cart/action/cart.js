fedc9702b88dfbfc9fbac0365b43365cc9ded38e
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2020-09-02 00:41:05 +0430

    openModalItem

--- src/redux/cart/action/cart.js
+++ src/redux/cart/action/cart.js
@@ -1,10 +1,17 @@
-const {
+import {
   ADD_TO_CART,
   REMOVE_ITEM_FROM_CART,
   DELETE_CART,
-} = require('../../../constant/types');
-
+  OPEN_MODAL_ITEM,
+} from '../../../constant/types';
 export const addToCart = (item) => {
+  // const itemArray = [];
+  // const arryKeys = Object.keys(item);
+  // const arryValues = Object.values(item);
+  // for (let i = 0; i < arryKeys.length; i++) {
+  //   return (itemArray[arryKeys[i]] = arryValues[i]);
+  // }
+  // console.log(itemArray.length);
   return {
     type: ADD_TO_CART,
     payload: item,
@@ -21,6 +28,13 @@ export const removeFromCart = (item) => {
 export const deleteCart = (item) => {
   return {
     type: DELETE_CART,
+    payload: item,
+  };
+};
+
+export const openModalItem = (item) => {
+  return {
+    type: OPEN_MODAL_ITEM,
     payload: item,
   };
 };
