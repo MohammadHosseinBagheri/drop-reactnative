25af805fd43480fc8fde5fbbf087b2838d8a831e
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2020-09-02 00:41:45 +0430

    add to cart and open modal

--- src/redux/cart/reducer/cart.js
+++ src/redux/cart/reducer/cart.js
@@ -2,19 +2,27 @@ import {
   ADD_TO_CART,
   REMOVE_ITEM_FROM_CART,
   DELETE_CART,
+  OPEN_MODAL_ITEM,
 } from '../../../constant/types';
 import {addToCart, removeFromCart, deleteCartItem} from '../cart-utils';
 
 const INITIAL_STATE = {
-  addedToCart: null,
+  addedToCart: [],
+  openModalItem: '',
 };
+
 
 const cartReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case OPEN_MODAL_ITEM:
+      return {
+        ...state,
+        openModalItem: action.payload,
+      };
     case ADD_TO_CART:
       return {
         ...state,
-        addedToCart: addToCart(state.addedToCart, action.payload),
+        addedToCart:  addToCart(state.addedToCart, action.payload),
       };
     case REMOVE_ITEM_FROM_CART:
       return {
