eb0d878dc2e9a4b8781fc0c99b96c8e0b4c9556d
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2020-09-02 00:32:05 +0430

    refactor

---
+++ src/components/app-content/AppContent.js
@@ -0,0 +1,42 @@
+import React, {useRef} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import MyHeader from '../header/Haeder';
+import Routes from '../../routes';
+import AddToCartModal from '../add-to-cart-modal/AddToCartModal';
+import ShoppingCart from '../shopping-cart/ShoppingCart';
+import {connect} from 'react-redux';
+import {Icon} from 'native-base';
+import EmptyShoppingCart from '../empty-shopping-cart/EmptyShoppingCart';
+const AppContent = ({addedItem}) => {
+  const addTocartModal = useRef(null);
+
+  const open = () => {
+    addTocartModal.current.open();
+  };
+  return (
+    <>
+      <MyHeader
+        backgroundColor={'red'}
+        body={<Text style={styles.header}>Demo App</Text>}
+      />
+      {/* <Home open={open} /> */}
+      <Routes open={open} />
+      {/* <Button onPress={() => open()}>
+        <Text>open</Text>
+      </Button> */}
+      <AddToCartModal ref={addTocartModal} />
+      {addedItem.length == 0 ? <EmptyShoppingCart /> : <ShoppingCart />}
+    </>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    addedItem: state.cart.addedToCart,
+  };
+};
+export default connect(mapStateToProps)(AppContent);
+
+const styles = StyleSheet.create({
+  header: {fontSize: 20, fontWeight: 'bold'},
+});
