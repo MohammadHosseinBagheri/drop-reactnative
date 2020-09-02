b46f334aab04a5c15c5b0a9c9eef8bce0efc15c1
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2020-09-02 20:10:53 +0430

    remove sth

--- App.js
+++ App.js
@@ -1,25 +1,9 @@
-import React, {useRef, useState} from 'react';
-import {
-  ScrollView,
-  Dimensions,
-  View,
-  Text,
-  TouchableOpacity,
-} from 'react-native';
+import React from 'react';
+
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
 import logger from 'redux-logger';
 import reducer from './src/redux/root-reducer';
-
-import createSagaMiddleware from 'redux-saga';
-
-import MyHeader from './src/components/header/Haeder';
-import AddToCartModal from './src/components/add-to-cart-modal/AddToCartModal';
-import Home from './src/routes/home-screen/Home';
-import rootSaga from './src/redux/sagas/root-saga';
-import Routes from './src/routes';
-import ShoppingCart from './src/components/shopping-cart/ShoppingCart';
-import {Button} from 'native-base';
 import AppContent from './src/components/app-content/AppContent';
 import {persistStore, persistReducer} from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
