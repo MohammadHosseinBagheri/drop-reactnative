48b9db264f96289799456b01dbf82d7b9dce8f7f
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2020-09-02 20:19:29 +0430

    refactor and styling

--- src/components/empty-shopping-cart/EmptyShoppingCart.js
+++ src/components/empty-shopping-cart/EmptyShoppingCart.js
@@ -1,44 +1,14 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {Icon} from 'native-base';
+
 const EmptyShoppingCart = () => {
   return (
-    <View
-      style={{
-        height: 30,
-        backgroundColor: '#37474F',
-        borderTopRightRadius: 15,
-        borderTopLeftRadius: 15,
-      }}>
-      <View
-        style={{
-          height: 2,
-          width: 20,
-          borderStyle: 'solid',
-          borderWidth: 1,
-          borderColor: '#fff',
-          alignSelf: 'center',
-          marginTop: 4,
-        }}></View>
-      <View
-        style={{
-          flexDirection: 'row',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}>
-        <Icon
-          name="basket"
-          style={{color: '#fff', fontSize: 14, marginHorizontal: 5}}
-        />
-        <Text
-          style={{
-            textAlign: 'center',
-            fontSize: 14,
-            color: '#fff',
-            fontWeight: '700',
-          }}>
-          Shopping Cart
-        </Text>
+    <View style={styles.container}>
+      <View style={styles.lineView}></View>
+      <View style={styles.titleView}>
+        <Icon name="basket" style={styles.icon} />
+        <Text style={styles.title}>Shopping Cart</Text>
       </View>
     </View>
   );
@@ -46,4 +16,32 @@ const EmptyShoppingCart = () => {
 
 export default EmptyShoppingCart;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    height: 30,
+    backgroundColor: '#37474F',
+    borderTopRightRadius: 15,
+    borderTopLeftRadius: 15,
+  },
+  lineView: {
+    height: 2,
+    width: 20,
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderColor: '#fff',
+    alignSelf: 'center',
+    marginTop: 4,
+  },
+  titleView: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  icon: {color: '#fff', fontSize: 14, marginHorizontal: 5},
+  title: {
+    textAlign: 'center',
+    fontSize: 14,
+    color: '#fff',
+    fontWeight: '700',
+  },
+});
