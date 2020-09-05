346312365e96f63aeb6d9e16fd5e14aad03079a7
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2020-09-05 20:34:31 +0430

    show cart action

--- src/constant/types.js
+++ src/constant/types.js
@@ -6,3 +6,4 @@ export const ADD_TO_CART = 'ADD_TO_CART';
 export const REMOVE_ITEM_FROM_CART = 'REMOVE_ITEM_FROM_CART';
 export const DELETE_CART = 'DELETE_CART';
 export const OPEN_MODAL_ITEM = 'OPEN_MODAL_ITEM';
+export const SHOW_CART = 'SHOW_CART';
