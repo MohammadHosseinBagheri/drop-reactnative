2d2f61f1c0e8fc6f2e9467aa69922fed96cdd9f2
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2020-09-05 20:18:05 +0430

    force ltr

--- android/app/src/main/java/com/challenge/MainApplication.java
+++ android/app/src/main/java/com/challenge/MainApplication.java
@@ -11,6 +11,8 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.facebook.react.modules.i18nmanager.I18nUtil;
+
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -46,6 +48,10 @@ public class MainApplication extends Application implements ReactApplication {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
     initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+    
+I18nUtil sharedI18nUtilInstance = I18nUtil.getInstance();
+sharedI18nUtilInstance.forceRTL(this,false);
+sharedI18nUtilInstance.allowRTL(this, false);
   }
 
   /**
